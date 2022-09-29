77ff10d48af470064d8c598af9680226823d1f96
Theanchhing <45730153+Theanchhing@users.noreply.github.com>
2022-09-29 19:03:58 +0700

    refactor: add age range to setting

--- src/documents/setting.document.ts
+++ src/documents/setting.document.ts
@@ -1,12 +1,13 @@
-import mongoose from "mongoose"
-import { UserDocument } from "./users.document"
+import mongoose from "mongoose";
+import { UserDocument } from "./users.document";
 
-export interface SettingDocument extends mongoose.Document{
-    user: UserDocument['_id']
-    find_distance: string
-    find_age: string
-    find_sex: string
-    language: string
-    createdAt: Date
-    updatedAt: Date
+export interface SettingDocument extends mongoose.Document {
+  user: UserDocument["_id"];
+  find_distance: string;
+  find_age_min: number;
+  find_age_max: number;
+  find_sex: string;
+  language: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
--- src/models/setting.model.ts
+++ src/models/setting.model.ts
@@ -1,20 +1,20 @@
 import mongoose from "mongoose";
 import { SettingDocument } from "../documents/setting.document";
 
-
 const settingSchema = new mongoose.Schema(
-    {
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-        find_distance: { type: String, required: true },
-        find_age: { type: String, required: true },
-        find_sex: { type: String, required: true },
-        language: { type: String, required: true },
-    },
-    {
-        timestamps: true
-    }
-)
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    find_distance: { type: String, required: true },
+    find_age_min: { type: Number, required: true, default: 18 },
+    find_age_max: { type: Number, required: true, default: 50 },
+    find_sex: { type: String, required: true },
+    language: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const settingModel = mongoose.model<SettingDocument>("Setting", settingSchema)
+const settingModel = mongoose.model<SettingDocument>("Setting", settingSchema);
 
-export default settingModel
+export default settingModel;
